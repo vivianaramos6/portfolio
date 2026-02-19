088aaafd373f12bce05caab62f153c441475586a
vivianaramos6 <viviana.ramos6@upr.edu>
2026-02-19 17:07:26 -0400

    Added to json

--- src/components/CircularGallery.jsx
+++ src/components/CircularGallery.jsx
@@ -225,7 +225,7 @@ class Media {
       transparent: true,
     });
     const img = new Image();
-    img.crossOrigin = "anonymous";
+    // img.crossOrigin = "anonymous";
     img.src = this.image;
     img.onload = () => {
       texture.image = img;
